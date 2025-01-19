0e660b622274e44b0f9d50a3931f1a75048d803d
abebediye <abebe.bediye@gmail.com>
2025-01-19 23:59:11 +0100

    remove demo and demo.test

--- uke02/caesar-cipher/src/demo.test.js
+++
@@ -1,8 +0,0 @@
-import { inverseBoolean } from "./demo";
-
-describe("test boolean inverser", () => {
-    test("inverseBoolean should return false when input is true", () => {
-        const input = true;
-        expect(inverseBoolean(input)).toBe(false);
-    });
-});
