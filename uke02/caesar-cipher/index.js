d8f91592a92dc52a0c0f3d020a163e8d99ca725e
abebediye <abebe.bediye@gmail.com>
2025-01-20 23:59:59 +0100

    implement decode action and add tests

--- uke02/caesar-cipher/index.js
+++ uke02/caesar-cipher/index.js
@@ -3,9 +3,14 @@ const source = document.getElementById("input");
 const output = document.getElementById("output");
 const encodeBtn = document.getElementById("encode");
 const decodeBtn = document.getElementById("decode");
-const shiftKye = document.getElementById("shift-key");
+const shiftKey = document.getElementById("shift-key");
 
 encodeBtn.addEventListener("click", () => {
     console.log("input", source.value);
-    output.value = encode(source.value, shiftKye.value);
+    output.value = encode(source.value, shiftKey.value);
+});
+
+decodeBtn.addEventListener("click", () => {
+    console.log("input", source.value);
+    output.value = decode(source.value, shiftKey.value);
 });
