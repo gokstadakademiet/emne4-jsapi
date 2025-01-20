c72a073366f915ecefea4bb027a4d69dff821f59
abebediye <abebe.bediye@gmail.com>
2025-01-20 23:48:59 +0100

    update configs to also handle modules in html

--- uke02/caesar-cipher/jest.config.js
+++ uke02/caesar-cipher/jest.config.js
@@ -174,7 +174,7 @@ const config = {
     // testRunner: "jest-circus/runner",
 
     // A map from regular expressions to paths to transformers
-    transform: { "\\.[jt]sx?$": "babel-jest" },
+    transform: {},
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
