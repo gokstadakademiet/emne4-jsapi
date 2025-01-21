8607099dd1f3db7a73cb26a808e4b6661c42d03c
abebediye <abebe.bediye@gmail.com>
2025-01-21 09:28:29 +0100

    fix test configurations for jest to run

--- uke02/caesar-cipher/index.js
+++ uke02/caesar-cipher/index.js
@@ -1,4 +1,4 @@
-import { encode } from "./src/cipher/caesar.js";
+import { decode, encode } from "./src/cipher/caesar.js";
 const source = document.getElementById("input");
 const output = document.getElementById("output");
 const encodeBtn = document.getElementById("encode");
@@ -6,11 +6,9 @@ const decodeBtn = document.getElementById("decode");
 const shiftKey = document.getElementById("shift-key");
 
 encodeBtn.addEventListener("click", () => {
-    console.log("input", source.value);
     output.value = encode(source.value, shiftKey.value);
 });
 
 decodeBtn.addEventListener("click", () => {
-    console.log("input", source.value);
     output.value = decode(source.value, shiftKey.value);
 });
