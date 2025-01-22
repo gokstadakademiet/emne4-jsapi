e1b64e31ae8183cc793c20be6cb53cd21d81e8fb
abebediye <abebe.bediye@gmail.com>
2025-01-22 12:58:52 +0100

    implement encode support for norwegian letters

--- uke02/caesar-cipher/src/cipher/caesar.js
+++ uke02/caesar-cipher/src/cipher/caesar.js
@@ -5,22 +5,35 @@ export const decode = (cipher, key) => {
 export const encode = (plain, key) => {
     const characters = [...plain];
     const shiftedCharacters = characters.map((char) => {
-        if (!isAlphabet(char)) {
-            return char;
+        if (isAlphabet(char)) {
+            return shiftLetter(char, key % 26);
         }
-        return shiftLetter(char, key % 26);
+        if ("æøåÆØÅ".includes(char)) {
+            return shiftNorwegianLetter(char, key % 5);
+        }
+        return char;
     });
     return shiftedCharacters.join("");
 };
 
 const shiftLetter = (letter, key) => {
     const isLowerCase = letter === letter.toLowerCase();
+
     const code = isLowerCase
         ? letter.toUpperCase().charCodeAt(0)
         : letter.charCodeAt(0);
     const shiftedLetter = String.fromCharCode(
         code + key > 90 ? code + key - 26 : code + key
     );
+    return isLowerCase ? shiftedLetter.toLowerCase() : shiftedLetter;
+};
+
+const shiftNorwegianLetter = (letter, key) => {
+    const isLowerCase = letter === letter.toLowerCase();
+    const code = isLowerCase
+        ? letter.toUpperCase().charCodeAt(0)
+        : letter.charCodeAt(0);
+    const shiftedLetter = String.fromCharCode(code + key);
     return isLowerCase ? shiftedLetter.toLowerCase() : shiftedLetter;
 };
 
