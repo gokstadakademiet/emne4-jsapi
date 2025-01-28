5511430a19bc8d6832d8f9c67fafb55568545497
abebediye <abebe.bediye@gmail.com>
2025-01-28 15:14:47 +0100

    implement and test authentication function

--- uke03/authenticator/src/authenticate.js
+++ uke03/authenticator/src/authenticate.js
@@ -1,3 +1,4 @@
+import { getUsers } from "../utils/usersMock.js";
 export const isAuthenticated = () => {
     const userString = localStorage.getItem("user");
     const user = JSON.parse(userString);
@@ -11,4 +12,29 @@ export const isAuthenticated = () => {
     }
 
     return true;
+};
+
+export const authenticate = (username, password) => {
+    // Check if username or password is empty
+    if (username === "" || password === "") {
+        return false;
+    }
+
+    // fetch list of existing users
+    const users = getUsers();
+
+    // Find user with same username in the list of users
+    const user = users.find((user) => user.username === username);
+
+    // Check if any user was found
+    if (!user) {
+        return false;
+    }
+    // Check if password is correct
+
+    if (user.password !== password) {
+        return false;
+    }
+
+    return user;
 };
