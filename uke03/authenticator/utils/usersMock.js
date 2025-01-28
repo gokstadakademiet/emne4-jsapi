e5ce0c9009b58b4c646e262025159ab86ac4df76
abebediye <abebe.bediye@gmail.com>
2025-01-28 14:10:04 +0100

    add usersMock

---
+++ uke03/authenticator/utils/usersMock.js
@@ -0,0 +1,30 @@
+export const users = [
+    {
+        id: 1,
+        username: "johndoe",
+        password: "password",
+        name: "John Doe",
+        email: "johndoe@example.com",
+    },
+    {
+        id: 2,
+        username: "janedoe",
+        password: "password",
+        name: "Jane Doe",
+        email: "janedoe@example.com",
+    },
+    {
+        id: 3,
+        username: "alicebaker",
+        password: "password",
+        name: "Alice Baker",
+        email: "alicebaker@example.com",
+    },
+    {
+        id: 4,
+        username: "bobcarlson",
+        password: "password",
+        name: "Bob Carlson",
+        email: "bobcarlson@example.com",
+    },
+];
