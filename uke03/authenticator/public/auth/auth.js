ae0138a1b77dcd9fa802fc431e8d1b2c58bf4dfb
abebediye <abebe.bediye@gmail.com>
2025-01-28 17:40:38 +0100

    implement eventlistner for form
    - submit listner for capturing submit event
    - formdata for capturing formdata when submitted.

---
+++ uke03/authenticator/public/auth/auth.js
@@ -0,0 +1,21 @@
+import { authenticate } from "../../src/authenticate.js";
+
+const signinForm = document.querySelector('form[name="signin"]');
+const singinError = document.querySelector("label[for='signin']");
+
+signinForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    new FormData(signinForm);
+});
+
+signinForm.addEventListener("formdata", (event) => {
+    const data = Object.fromEntries(event.formData);
+    const user = authenticate({ ...data });
+
+    if (!user) {
+        singinError.textContent = "Invalid username or password";
+        return;
+    }
+
+    localStorage.setItem("user", JSON.stringify(user));
+});
