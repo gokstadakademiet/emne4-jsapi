3fc6d01e41f82e6673c4654db126b4b5c29e42a2
abebediye <abebe.bediye@gmail.com>
2025-01-28 16:18:26 +0100

    implement and test register new user functions

--- uke03/authenticator/src/authenticate.js
+++ uke03/authenticator/src/authenticate.js
@@ -1,4 +1,4 @@
-import { getUsers } from "../utils/usersMock.js";
+import { getUsers, registerUser } from "../utils/usersMock.js";
 export const isAuthenticated = () => {
     const userString = localStorage.getItem("user");
     const user = JSON.parse(userString);
@@ -37,4 +37,33 @@ export const authenticate = (username, password) => {
     }
 
     return user;
+};
+
+export const register = (newUser) => {
+    // Check if username or password is empty
+
+    const { username, password, name, email } = newUser;
+    if (username === "" || password === "") {
+        return false;
+    }
+
+    if (email === "" || name === "") {
+        return false;
+    }
+
+    // fetch list of existing users
+    const existingUsers = getUsers();
+
+    // Check if username is already taken
+    const existingUser = existingUsers.find(
+        (user) => user.username === username
+    );
+
+    // Check if any user was found return error
+    if (existingUser) {
+        return false;
+    }
+
+    // Add new user to the list
+    registerUser(newUser);
 };
